795572c0cdf80e576b1ea343fc9f2c118fe8d349
Naveen Kumar Uppuluri <nuppulur@cisco.com>
2016-08-11 21:44:57 -0700

    Implemented Create and List using Doctrine

--- src/AppBundle/Controller/UrlController.php
+++ src/AppBundle/Controller/UrlController.php
@@ -3,37 +3,67 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Url;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UrlController extends Controller
 {
     /**
-     * @Route("/url", name="url")
+     * @Route("/url", name="url_index")
      */
     public function indexAction()
     {
-        $response_code = "";
-        $url = "";
-        $output = "";
-        if(isset($_POST['url'])) {
-            $url = $_POST['url'];
+        $urls = $this->getDoctrine()
+            ->getRepository('AppBundle:Url')
+            ->findAll();
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
-            $output = curl_exec($ch);
-            $response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-
-        }
-        // replace this example code with whatever you need
         return $this->render('url/index.html.twig', [
-            'response_code' => $response_code,
-            'url' => $url,
-            'output' => $output,
+            'urls' => $urls,
 
+        ]);
+    }
+
+    /**
+     * @Route("/url/create", name="url_create")
+     */
+    public function createAction(Request $request) {
+        $url = new Url();
+
+        $form = $this->createFormBuilder($url)
+            -> add('title',TextType::class,array('attr'=> array('class'=>'form-control','style'=>'margin-bottom:15px')))
+            -> add('domain',TextType::class,array('attr'=> array('class'=>'form-control','style'=>'margin-bottom:15px')))
+            -> add('url',TextType::class,array('attr'=> array('class'=>'form-control','style'=>'margin-bottom:15px')))
+            -> add('status',ChoiceType::class,array('choices'=>array('Disabled'=>'Disabled','Enabled'=>'Enabled'),'attr'=> array('class'=>'form-control','style'=>'margin-bottom:15px')))
+            -> add('body',TextareaType::class,array('attr'=> array('class'=>'form-control','style'=>'margin-bottom:15px')))
+            -> add('save',SubmitType::class,array('attr'=> array('class'=>'btn btn-primary')))
+            -> getForm();
+
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted() && $form->isValid()){
+
+            $url->setTitle($form['title']->getData());
+            $url->setDomain($form['domain']->getData());
+            $url->setUrl($form['url']->getData());
+            $url->setStatus($form['status']->getData());
+            $url->setBody($form['body']->getData());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($url);
+            $em->flush();
+
+            $this->addFlash('success','Url Added');
+
+            return $this->redirectToRoute('url_index');
+        }
+        return $this->render('url/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/AppBundle/Entity/Url.php
+++ src/AppBundle/Entity/Url.php
@@ -5,42 +5,216 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * Url
+ *
  * @ORM\Table(name="url")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UrlRepository")
  */
 class Url
 {
     /**
-     * @ORM\Column(type="integer")
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
      */
     private $title;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @var string
+     *
+     * @ORM\Column(name="domain", type="string", length=255)
      */
     private $domain;
 
     /**
-     * @ORM\Column(type="string", length=250)
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
      */
     private $url;
 
     /**
-     * @ORM\Column(type="integer")
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=255)
      */
     private $status;
 
     /**
-     * @ORM\Column(type="text")
+     * @var string
+     *
+     * @ORM\Column(name="body", type="text")
      */
     private $body;
 
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_on", type="datetimetz", nullable=true)
+     */
+    private $createdOn;
 
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Url
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set domain
+     *
+     * @param string $domain
+     *
+     * @return Url
+     */
+    public function setDomain($domain)
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    /**
+     * Get domain
+     *
+     * @return string
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return Url
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     *
+     * @return Url
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set createdOn
+     *
+     * @param \DateTime $createdOn
+     *
+     * @return Url
+     */
+    public function setCreatedOn($createdOn)
+    {
+        $this->createdOn = $createdOn;
+
+        return $this;
+    }
+
+    /**
+     * Get createdOn
+     *
+     * @return \DateTime
+     */
+    public function getCreatedOn()
+    {
+        return $this->createdOn;
+    }
 }
